5eea076af730ce2bfae8a211e31f9a2775eca455
risv1 <rishi.aangarai@gmail.com>
2025-04-13 08:55:47 +0530

    feat: init smp

--- apps/backend/smp-service/src/index.ts
+++ apps/backend/smp-service/src/index.ts
@@ -1,15 +1,28 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 
+import { env } from './config/env.ts'
+import logger from './config/logger.ts'
+import router from './api/routes/routes.ts'
+import loggingMiddleware from './api/middlewares/logging.middleware.ts'
+
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.use(loggingMiddleware)
+
+app.route("/api", router)
+
+app.notFound(async(c) => {
+  return c.json({
+    status: 404,
+    message: 'Not Found',
+    prettyMessage: "The requested resource was not found",
+  }, 404)
 })
 
 serve({
   fetch: app.fetch,
-  port: 3000
+  port: env.PORT,
 }, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
+  logger.info(`Server is running on http://localhost:${info.port}`)
 })
